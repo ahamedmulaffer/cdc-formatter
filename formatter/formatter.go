19d03b42365ab7cdbdf603ac0a340a144c9778e3
ahamedmulaffer <mulaffer1992@gmail.com>
2021-09-12 16:57:09 +0400

    commented a message

--- formatter/formatter.go
+++ formatter/formatter.go
@@ -30,14 +30,11 @@ func Process(message string) (interface{}, error) {
 		return nil, err
 	}
 	connectorStr := currentMessageSourceMap["connector"].(string)
-	// fmt.Println(connectorStr)
 	res, err := call(connectorStr, currentMessagePayloadMap, currentMessageSourceMap)
 	if err != nil {
 		return nil, err
 	}
 	return res[0].Interface().(map[string]map[string]interface{}), err
-	//this res will have actual error
-	// you can get it like res[0].Interface()
 }
 
 func call(funcName string, params ... interface{}) (res []reflect.Value, err error) {
@@ -47,25 +44,21 @@ func call(funcName string, params ... interface{}) (res []reflect.Value, err err
     //     }
     // }()
 	f := reflect.ValueOf(funcMapper[funcName])
-	// fmt.Println(len(params), f.Type().NumIn())
 	if len(params) != f.Type().NumIn() {
 		err = errors.New("The number of params is out of index.")
 		return nil, err
 	}
 	in := make([]reflect.Value, len(params))
 	for k, param := range params {
-		// fmt.Println()
-		// fmt.Println(param)
 		in[k] = reflect.ValueOf(param)
 	}
-	// fmt.Println(in)
 	res = f.Call(in)
 	return res, nil
 }
 
 func messageUnMarshaller(message string, messageMap map[string]interface{}) error{
 	if err := json.Unmarshal([]byte(message), &messageMap); err != nil {
-		fmt.Println("Error in Unmarshaling at currentMessage:", err)
+		// fmt.Println("Error in Unmarshaling at currentMessage:", err)
 		return err
 	}
 	return nil
