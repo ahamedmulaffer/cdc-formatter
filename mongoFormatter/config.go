d5a5755e338d23276bd9b5f53e24430a99ba7462
ahamedmulaffer <mulaffer1992@gmail.com>
2022-04-11 20:45:00 +0400

    mongo update format coming in after not in before in debezium connector 1.8.0

--- mongoFormatter/config.go
+++ mongoFormatter/config.go
@@ -1,9 +1,10 @@
 package mongoFormatter
-import(
+
+import (
 	"encoding/json"
 	"errors"
-	"regexp"
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -19,52 +20,51 @@ var collectionRegex = make(map[string]map[string]string)
 var hostIdWithCollectioOperation = make(map[string]map[string]string)
 
 var allowedKeys = map[string]bool{
-	"insert": true,
-	"update": true,
-	"delete": true,
-	"regex": true,
-	"required_after_all": true,
-	"required_after_fields": true,
-	"required_before_fields": true,
-	"underscore_with_hostid_fields": true,
+	"insert":                              true,
+	"update":                              true,
+	"delete":                              true,
+	"regex":                               true,
+	"required_after_all":                  true,
+	"required_after_fields":               true,
+	"required_before_fields":              true,
+	"underscore_with_hostid_fields":       true,
 	"underscore_with_hostid_array_fields": true,
-	"all_value_to_string": true,
-	"null_to": true,
-	"host_id":true,
+	"all_value_to_string":                 true,
+	"null_to":                             true,
+	"host_id":                             true,
 }
 
-var stringBooleanKeys = map[string]string {
-	"required_after_all": "true",
+var stringBooleanKeys = map[string]string{
+	"required_after_all":  "true",
 	"all_value_to_string": "true",
 }
 
-var commaSeperatedkeys = map[string]bool {
-	"required_after_fields": true,
-	"required_before_fields": true,
-	"underscore_with_hostid_fields": true,
+var commaSeperatedkeys = map[string]bool{
+	"required_after_fields":               true,
+	"required_before_fields":              true,
+	"underscore_with_hostid_fields":       true,
 	"underscore_with_hostid_array_fields": true,
 }
 
 var splitToSliceFieldsFrmString = map[string]map[string]map[string][]string{
-	"required_after_fields": allRequiredAfterFields,
+	"required_after_fields":  allRequiredAfterFields,
 	"required_before_fields": allRequiredBeforeFields,
 }
 
 var splitToMapFieldsFrmString = map[string]map[string]map[string]map[string]string{
-	"underscore_with_hostid_fields": allUnderscoreWithHostidFields,
+	"underscore_with_hostid_fields":       allUnderscoreWithHostidFields,
 	"underscore_with_hostid_array_fields": allUnderscoreWithHostidArrayFields,
 }
 
-
-var strValuedKeys = map[string]bool {
-	"required_after_all": true,
-	"required_after_fields": true,
-	"required_before_fields": true,
-	"underscore_with_hostid_fields": true,
+var strValuedKeys = map[string]bool{
+	"required_after_all":                  true,
+	"required_after_fields":               true,
+	"required_before_fields":              true,
+	"underscore_with_hostid_fields":       true,
 	"underscore_with_hostid_array_fields": true,
-	"all_value_to_string": true,
-	"null_to": true,
-	"host_id": true,
+	"all_value_to_string":                 true,
+	"null_to":                             true,
+	"host_id":                             true,
 }
 
 var singleValuedKeys = map[string]string{
@@ -83,16 +83,16 @@ func Register(config string) error {
 	return nil
 }
 
-func unMarshallConfig(config string) error{
+func unMarshallConfig(config string) error {
 	if err := json.Unmarshal([]byte(config), &configMap); err != nil {
 		return err
 	}
 	return nil
 }
 
-func validateConfig() error{
+func validateConfig() error {
 	emptyConfig := true
-	for collectionName,configVal := range configMap {
+	for collectionName, configVal := range configMap {
 		emptyConfig = false
 		if err := isValidOperationExist(collectionName, configMap[collectionName]); err != nil {
 			return err
@@ -133,24 +133,24 @@ func validateConfig() error{
 	return nil
 }
 
-func isNotAnOperation(operation string) bool{
+func isNotAnOperation(operation string) bool {
 	if operation != "insert" && operation != "update" && operation != "delete" {
 		return true
 	}
 	return false
 }
 
-func isRegexExist(collectionName string, dataMap map[string]map[string]interface{}) error{
-	if _,ok := dataMap["regex"]; ok {
+func isRegexExist(collectionName string, dataMap map[string]map[string]interface{}) error {
+	if _, ok := dataMap["regex"]; ok {
 		if len(dataMap["regex"]) != 1 {
 			return errors.New("object regex only can have one key")
 		}
-		for k,v := range dataMap["regex"]{
+		for k, v := range dataMap["regex"] {
 			val, ok := canBeAssertToString(v)
 			if !ok {
 				return errors.New("object regex key value must be string")
 			}
-			if val != "startsWith" &&  val != "endsWith" && val != "contains" && val != "equals" {
+			if val != "startsWith" && val != "endsWith" && val != "contains" && val != "equals" {
 				return errors.New("object regex key's value must be startsWith|endsWith|contains|equals")
 			}
 			if _, ok := collectionRegex[collectionName]; !ok {
@@ -162,29 +162,29 @@ func isRegexExist(collectionName string, dataMap map[string]map[string]interface
 	return nil
 }
 
-func isValidOperationExist(collectionName string, operations map[string]map[string]interface{}) error{
+func isValidOperationExist(collectionName string, operations map[string]map[string]interface{}) error {
 	_, insertOk := operations["insert"]
 	_, updateOk := operations["update"]
 	_, deleteOk := operations["delete"]
 	if !insertOk && !updateOk && !deleteOk {
-		return errors.New("collection "+collectionName+ " need atlest insert|update|delete operation to be mentioned")
+		return errors.New("collection " + collectionName + " need atlest insert|update|delete operation to be mentioned")
 	}
 	return nil
-} 
+}
 
 func isValidKey(operation string, key string) error {
 	if _, ok := allowedKeys[key]; !ok {
-		return errors.New("key "+key+ " is not allowed in configurations")
+		return errors.New("key " + key + " is not allowed in configurations")
 	}
 	if key == "required_before_fields" && operation == "insert" {
-		return errors.New("key "+key+ " is not allowed for operation "+operation)
+		return errors.New("key " + key + " is not allowed for operation " + operation)
 	}
 	if key == "required_after_all" && operation != "insert" {
-		return errors.New("key "+key+ " is not allowed for operation "+operation)
+		return errors.New("key " + key + " is not allowed for operation " + operation)
 	}
-	if key == "required_after_fields" && operation != "insert" {
-		return errors.New("key "+key+ " is not allowed for operation "+operation)
-	}
+	// if key == "required_after_fields" && operation != "insert" {
+	// 	return errors.New("key "+key+ " is not allowed for operation "+operation)
+	// }
 	return nil
 }
 
@@ -207,7 +207,7 @@ func onlyStringValuesAllowed(collection string, operation string, key string, v 
 	}
 	val, ok := v.(string)
 	if !ok {
-		return errors.New(key+" value should be string")
+		return errors.New(key + " value should be string")
 	}
 	if err := emptyValuesNotAllowed(collection, operation, key, val); err != nil {
 		return err
@@ -223,10 +223,10 @@ func onlyStringValuesAllowed(collection string, operation string, key string, v 
 	return nil
 }
 
-func isSingleValuedKey(key string, val string) error{
+func isSingleValuedKey(key string, val string) error {
 	if value, ok := singleValuedKeys[key]; ok {
 		if val != value {
-			return errors.New(key+" value should be "+value)
+			return errors.New(key + " value should be " + value)
 		}
 	}
 	return nil
@@ -234,7 +234,7 @@ func isSingleValuedKey(key string, val string) error{
 
 func emptyValuesNotAllowed(collection string, operation string, key string, val string) error {
 	if strings.TrimSpace(val) == "" {
-		return errors.New(key+" value cant be empty")
+		return errors.New(key + " value cant be empty")
 	}
 	if err := isACommaSeperatedValue(collection, operation, key, val); err != nil {
 		return err
@@ -245,29 +245,29 @@ func emptyValuesNotAllowed(collection string, operation string, key string, val 
 func isAStrBoolKey(collection string, operation string, key string, val string) error {
 	if v, ok := stringBooleanKeys[key]; ok {
 		if v != val {
-			return errors.New(key+" value should be "+v)
+			return errors.New(key + " value should be " + v)
 		}
 		setBoolCollectionOperation(collection, operation, key)
 	}
 	return nil
 }
 
-func isACommaSeperatedValue(collection string, operation string, key string, val string) error{
+func isACommaSeperatedValue(collection string, operation string, key string, val string) error {
 	r := regexp.MustCompile(`^(\w+)(,\s*\w+)*$`)
 	if _, ok := commaSeperatedkeys[key]; ok {
 		if !r.MatchString(val) {
-			return errors.New(key+" value should be comma seperated")
+			return errors.New(key + " value should be comma seperated")
 		}
 		if varCS, ok := splitToSliceFieldsFrmString[key]; ok {
 			if _, ok := varCS[collection]; !ok {
 				varCS[collection] = make(map[string][]string)
 			}
 			varCS[collection][operation] = strings.Split(val, ",")
-			
+
 		}
 		if varCS, ok := splitToMapFieldsFrmString[key]; ok {
 			var hostFieldsMap = make(map[string]string)
-			for _,v := range strings.Split(val, ",") {
+			for _, v := range strings.Split(val, ",") {
 				hostFieldsMap[v] = v
 			}
 			if _, ok := varCS[collection]; !ok {
@@ -275,7 +275,7 @@ func isACommaSeperatedValue(collection string, operation string, key string, val
 			}
 			varCS[collection][operation] = hostFieldsMap
 		}
-	} 
+	}
 	return nil
 }
 
@@ -283,7 +283,7 @@ func ifHostIDAvailable(collection string, operation string, key string, v string
 	if key != "host_id" {
 		return
 	}
-	if _,ok := hostIdWithCollectioOperation[collection]; !ok {
+	if _, ok := hostIdWithCollectioOperation[collection]; !ok {
 		hostIdWithCollectioOperation[collection] = make(map[string]string)
 	}
 	hostIdWithCollectioOperation[collection][operation] = v
@@ -293,7 +293,7 @@ func ifNullToAvailable(collection string, operation string, key string, v string
 	if key != "null_to" {
 		return
 	}
-	if _,ok := allNullToCollectionOperations[collection]; !ok {
+	if _, ok := allNullToCollectionOperations[collection]; !ok {
 		allNullToCollectionOperations[collection] = make(map[string]string)
 	}
 	allNullToCollectionOperations[collection][operation] = v
@@ -301,21 +301,15 @@ func ifNullToAvailable(collection string, operation string, key string, v string
 
 func setBoolCollectionOperation(collection string, operation string, key string) {
 	if key == "all_value_to_string" {
-		if _,ok := allValToStrOperations[collection]; !ok {
+		if _, ok := allValToStrOperations[collection]; !ok {
 			allValToStrOperations[collection] = make(map[string]bool)
 		}
 		allValToStrOperations[collection][operation] = true
 	}
 	if key == "required_after_all" {
-		if _,ok := afterAllCollectionOperations[collection]; !ok {
+		if _, ok := afterAllCollectionOperations[collection]; !ok {
 			afterAllCollectionOperations[collection] = make(map[string]bool)
 		}
 		afterAllCollectionOperations[collection][operation] = true
 	}
 }
-
-
-
-
-
-
