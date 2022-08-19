b1aa1fe745f6c91eeee3cb6151408bd70143b5a4
ahamedmulaffer <mulaffer1992@gmail.com>
2022-08-19 11:14:41 +0400

    added actual_collection

--- mongoFormatter/processor.go
+++ mongoFormatter/processor.go
@@ -1,6 +1,6 @@
 package mongoFormatter
 
-import(
+import (
 	// "strings"
 	// "fmt"
 	"encoding/json"
@@ -12,17 +12,19 @@ var allowedOperations = map[string]string{
 	"d": "delete",
 }
 
-func Process(payload map[string]interface{}, source map[string]interface{}) map[string]map[string]interface{}{
+func Process(payload map[string]interface{}, source map[string]interface{}) map[string]map[string]interface{} {
 	finalPayload := make(map[string]map[string]interface{})
 	collectionName := "N/A"
 	operation := "N/A"
+	actualCollectionName := source["collection"].(string)
 	finalPayload["before"] = make(map[string]interface{})
 	finalPayload["after"] = make(map[string]interface{})
-	finalPayload["source"] = map[string]interface{} {
-		"database": "mongo",
-		"collection": collectionName,
-		"operation": operation,
-		"allowed": false,
+	finalPayload["source"] = map[string]interface{}{
+		"database":          "mongo",
+		"collection":        collectionName,
+		"actual_collection": actualCollectionName,
+		"operation":         operation,
+		"allowed":           false,
 	}
 	if _, ok := payload["op"]; !ok {
 		return finalPayload
@@ -33,8 +35,8 @@ func Process(payload map[string]interface{}, source map[string]interface{}) map[
 	operation = allowedOperations[payload["op"].(string)]
 	finalPayload["source"]["collection"] = collectionName
 	finalPayload["source"]["operation"] = operation
-	
-	//check collection and its operation registered
+
+	// check collection and its operation registered
 	if _, ok := configMap[collectionName]; !ok {
 		return finalPayload
 	}
@@ -42,20 +44,20 @@ func Process(payload map[string]interface{}, source map[string]interface{}) map[
 		return finalPayload
 	}
 	finalPayload["source"]["allowed"] = true
-	requiredAfterFields , requiredAfterFieldsAvailable := afterType.isRequiredFieldsAvailable(collectionName, operation)
-	requiredB4Fields , requiredB4FieldsAvailable := beforeType.isRequiredFieldsAvailable(collectionName, operation)
+	requiredAfterFields, requiredAfterFieldsAvailable := afterType.isRequiredFieldsAvailable(collectionName, operation)
+	requiredB4Fields, requiredB4FieldsAvailable := beforeType.isRequiredFieldsAvailable(collectionName, operation)
 	if payload["after"] != nil {
 		payloadAfter := make(map[string]interface{})
 		if err := json.Unmarshal([]byte(payload["after"].(string)), &payloadAfter); err != nil {
 			return finalPayload
 		}
-		if requiredAfterFieldsAvailable{
-			afterType.loopRequiredFields(collectionName,operation,requiredAfterFields,payloadAfter,finalPayload["after"])
+		if requiredAfterFieldsAvailable {
+			afterType.loopRequiredFields(collectionName, operation, requiredAfterFields, payloadAfter, finalPayload["after"])
 		}
 		if _, ok := afterAllCollectionOperations[collectionName][operation]; ok {
-			afterType.loopPayloadFields(collectionName,operation,payloadAfter,finalPayload["after"])
+			afterType.loopPayloadFields(collectionName, operation, payloadAfter, finalPayload["after"])
 		}
-		
+
 	}
 	//Before
 	if payload["filter"] != nil {
@@ -64,20 +66,20 @@ func Process(payload map[string]interface{}, source map[string]interface{}) map[
 			return finalPayload
 		}
 		if requiredB4FieldsAvailable {
-			beforeType.loopRequiredFields(collectionName,operation,requiredB4Fields,payloadBefore,finalPayload["before"])
+			beforeType.loopRequiredFields(collectionName, operation, requiredB4Fields, payloadBefore, finalPayload["before"])
 		}
 	}
 	// fmt.Println("finalPayload", finalPayload)
 	return finalPayload
 }
 
-func getCollectionName(source map[string]interface{}) string{
+func getCollectionName(source map[string]interface{}) string {
 	collName := source["collection"].(string)
 	_, ok := configMap[collName]
 	if ok {
 		return collName
 	}
-	for collectionName,v := range collectionRegex {
+	for collectionName, v := range collectionRegex {
 		for pattern, regex := range v {
 			res, _ := call(regex, collName, pattern)
 			if res[0].Interface().(bool) {
@@ -87,4 +89,3 @@ func getCollectionName(source map[string]interface{}) string{
 	}
 	return collName
 }
-
